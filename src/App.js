94598cb5b7af2da9bdd59df8b7d8893ed561b753
Chabri <cmd.18@icloud.com>
2022-01-21 05:50:09 -0800

    about

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 import './App.css';
-import CampsiteInfo from './components/CampsiteInfoComponent';
+
 
 const store = ConfigureStore();
 
